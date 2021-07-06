49c7d550a0daf9ec38d9395eabd1cbbc11680e4c
Dylan McAulay <dmcaulay97@gmail.com>
2021-07-06 19:41:50 -0400

    adding handlebars for login signup

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -8,12 +8,22 @@ const withAuth = require('../utils/auth');
 //login route
 router.get('/', async (req, res) => {
     try {
-        res.render('TESTLOGIN');
+        res.render('login', { signup: true });
     } catch (err) {
         console.error(err);
         res.status(500).json(err);
     }
 });
+
+//signup route
+router.get('/signup', async (req, res) => {
+    try {
+        res.render('signup', { signup: true })
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+    }
+})
 
 //Homepage route
 router.get('/homePage', async (req, res) => {
